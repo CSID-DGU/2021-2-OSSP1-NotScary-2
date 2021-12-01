1388064d34f4cf4b8a5c3b201cf0afed49dc875c
oceanofglitta <73862762+oceanofglitta@users.noreply.github.com>
2021-12-01 19:51:11 +0900

    Update: Push Notification

--- amsut1121/src/components/Pose.js
+++ amsut1121/src/components/Pose.js
@@ -1,55 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Component } from "react/cjs/react.production.min";
 import * as tf from "@tensorflow/tfjs";
 
-class Pose extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posture: "", //현재 자세 state
-    };
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
   }
+
+  const fireNotif = () => {
+    /* 권한 요청 부분 */
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          /* 권한을 요청받고 nofi를 생성해주는 부분 */
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      /* 권한이 있을때 바로 noti 생성해주는 부분 */
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+
+function Pose(pros) {
+    const [posture, setPosture] = useState("0");
   //서버로 5가지 좌표값을 통해 분석된 자세 받아오는 함수
 
-  classPosture = setInterval(() => {
-    console.log("버튼클릭");
-    const post = {
-      // //ShoulderDistance,EyeDistance,ShoulderEyeDistance,ShoulderSlope,EyeSlope
-      // //SD:-1, ED:-1, SED:-1, SS:-1, ES:-1 로 초기 값 설정해서 진행
-      // SD: 418,
-      // ED: 117,
-      // SED: 188.2727,
-      // SS: 0,
-      // ES: 0, //임의로 설정한 자세 값(1번 자세)
-    };
-
+/*
     fetch("http://localhost:4000/posture", {
       // /posture를 post를 통해 서버와 연동
-      method: "post",
+      method: "get",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(post),
     })
       .then((res) => res.json())
       .then((json) => {
-        this.setState({
-          posture: json.text, //서버로부터 받아온 자세로 현재 state 자세 업데이트
-        });
+          setPosture(posture+1);
       });
-  }, 5000);
-
-  render() {
+    */
+    
+    
+        
+    useEffect(() => {
+        let a;
+        if(posture==1) a = new Notification("거북목 자세입니다!");
+        else if(posture == 2) a = new Notification("턱을 괸 자세입니다!");
+    }, [posture])
+    
+    
+    
     return (
       <div className="pose">
-        {this.state.posture == 0 && "좋은 자세입니다."}
+            {posture == 0 && "바른 자세입니다."}
+            <button onClick={() => setPosture("0")}>바</button>
+            <button onClick={() => setPosture("1")}>거</button>
+            <button onClick={() => setPosture("2")}>턱</button>
+            
         <div style={{ color: "red" }}>
-          {this.state.posture == 1 && "거북목 자세입니다."}
-          {this.state.posture == 2 && "턱을 괸 자세입니다."}
+            {posture == 1 && "거북목 자세입니다."}
+            {posture == 2 && "턱을 괸 자세입니다."}
         </div>
       </div>
     );
-  }
+  
 }
 
 export default Pose;

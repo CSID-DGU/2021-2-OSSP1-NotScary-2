0d374c6b525c0376dcbb4f2e403599baa3a6257b
oceanofglitta <73862762+oceanofglitta@users.noreply.github.com>
2021-12-15 18:55:23 +0900

    Create poseHistory.js

---
+++ amsut1121/src/components/poseHistory.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+
+function poseHistory(pros) {
+  return (
+    <div>
+        temp
+    </div>
+  );
+}
+
+export default poseHistory;

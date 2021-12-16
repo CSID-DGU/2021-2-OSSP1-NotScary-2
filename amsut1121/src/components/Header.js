a70935451d12927e664e07c082bac2d402190261
Changjin Lee <changjini32@gmail.com>
2021-12-17 01:11:20 +0900

    Add files via upload

--- amsut1121/src/components/Header.js
+++ amsut1121/src/components/Header.js
@@ -4,7 +4,13 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
-      <NavLink className="h" to="/">
+      <NavLink
+        className="h"
+        to="/"
+        onClick={() => {
+          window.localStorage.clear();
+        }}
+      >
         {/* <NavLink className="h" to="/"> */}
         AMSUT
       </NavLink>

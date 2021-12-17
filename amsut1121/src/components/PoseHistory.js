f6ce7e0f0b7ef2dcf7dcb6fe1291fc6fe8302bad
Changjin Lee <changjini32@gmail.com>
2021-12-17 23:15:22 +0900

    Add files via upload

---
+++ amsut1121/src/components/PoseHistory.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import raw from "./history.txt";
+
+function PoseHistory(pros) {
+  const [getDatum, setDatum] = useState([]);
+
+  let textToArray = [];
+  useEffect(() => {
+    const data = fetch(raw)
+      .then((r) => r.text())
+      .then((text) => {
+        //console.log('text decoded: ', text);
+        textToArray = text.toString().split("\n");
+        const result = textToArray.map((item) => item.split(" "));
+        setDatum(result);
+      });
+  }, []);
+
+  const [getMoment, setMoment] = useState(moment());
+
+  const today = getMoment;
+  const firstWeek = today.clone().startOf("month").week();
+  const lastWeek =
+    today.clone().endOf("month").week() == 1
+      ? 53
+      : today.clone().endOf("month").week();
+
+  const calendarArr = () => {
+    let result = [];
+    let week = firstWeek;
+    for (week; week <= lastWeek; week++) {
+      result = result.concat(
+        <tr key={week} className="defaultTr">
+          {Array(7)
+            .fill(0)
+            .map((data, index) => {
+              let days = today
+                .clone()
+                .startOf("year")
+                .week(week)
+                .startOf("week")
+                .add(index, "day");
+              const matchDay = getDatum.find(
+                (data) => data[0] == days.format("YYYYMMDD")
+              );
+              if (moment().format("YYYYMMDD") === days.format("YYYYMMDD")) {
+                if (matchDay != undefined) {
+                  return (
+                    <td key={index} className="todayTd">
+                      <span>{days.format("D")}</span>
+                      <br />
+                      <br />
+                      <span>나쁜 자세 {matchDay[1]}회 검출!</span>
+                    </td>
+                  );
+                } else {
+                  return (
+                    <td key={index} className="todayTd">
+                      <span>{days.format("D")}</span>
+                    </td>
+                  );
+                }
+              } else if (days.format("MM") !== today.format("MM")) {
+                return (
+                  <td key={index} className="notThisMonthTd">
+                    <span>{days.format("D")}</span>
+                  </td>
+                );
+              } else {
+                if (matchDay != undefined) {
+                  return (
+                    <td key={index} className="badTd">
+                      <span>{days.format("D")}</span>
+                      <br />
+                      <br />
+                      <span>나쁜 자세 {matchDay[1]}회 검출!</span>
+                    </td>
+                  );
+                } else {
+                  return (
+                    <td key={index} className="defaultTd">
+                      <span>{days.format("D")}</span>
+                    </td>
+                  );
+                }
+              }
+            })}
+        </tr>
+      );
+    }
+    return result;
+  };
+
+  return (
+    <div>
+      {console.log("아")}
+      <div className="monthSelect-Wrapper">
+        <button
+          onClick={() => {
+            setMoment(getMoment.clone().subtract(1, "month"));
+          }}
+          className="leftRightButton"
+        >
+          ◀
+        </button>
+        <span style={{ fontWeight: "bold", fontSize: "6vh", marginTop: "2vh" }}>
+          {today.format("YYYY년 MM월")}
+        </span>
+        <button
+          onClick={() => {
+            setMoment(getMoment.clone().add(1, "month"));
+          }}
+          className="leftRightButton"
+        >
+          ▶
+        </button>
+      </div>
+      <table>
+        <tr>
+          <td style={{ color: "red" }}>일</td>
+          <td>월</td>
+          <td>화</td>
+          <td>수</td>
+          <td>목</td>
+          <td>금</td>
+          <td style={{ color: "blue" }}>토</td>
+        </tr>
+        <tbody>{calendarArr()}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default PoseHistory;
--- amsut1121/src/components/StartCheck2.js
+++ amsut1121/src/components/StartCheck2.js
@@ -3,29 +3,20 @@ import { Redirect } from "react-router-dom";
 import Webcam from "react-webcam";
 import goldman from "./goldman.png";
 
+var imageSrc;
 var count = 1;
 
 function StartCheck2() {
   var poseArr = [];
+  var interval;
 
   const [posture, setPosture] = useState(0);
 
   const SC = () => {
     const webcamRef = useRef();
-
     const capture = () => {
       setTimeout(function () {
-        var imageSrc;
-
-        console.log(webcamRef.current);
-
-        if (webcamRef.current == null)
-          imageSrc = "C:\\Users\\82109\\Downloads\\start_capture.jpg";
-        else imageSrc = webcamRef.current.getScreenshot();
-
-        console.log("asdf");
         fetch("http://localhost:4000/startcheck", {
-          // /posture를 post를 통해 서버와 연동
           method: "post",
           headers: {
             "content-type": "application/json",
@@ -36,6 +27,29 @@ function StartCheck2() {
             setPosture(json.text);
             poseArr = json.text.split("#");
           });
+      }, 10);
+
+      if (poseArr[0] == "-1") setPosture(-1);
+
+      if (poseArr[0] == "1" && count) {
+        console.log("좋은 자세");
+        window.localStorage.setItem("start", poseArr[0]);
+        window.localStorage.setItem("p1", poseArr[1]);
+        window.localStorage.setItem("p2", poseArr[2]);
+        window.localStorage.setItem("p3", poseArr[3]);
+        window.localStorage.setItem("p4", poseArr[4]);
+        window.localStorage.setItem("p5", poseArr[5]);
+
+        clearInterval(interval);
+        count = 0;
+      }
+    };
+
+    useEffect(() => {
+      interval = setInterval(() => {
+        if (webcamRef.current == null)
+          imageSrc = "C:\\Users\\82109\\Downloads\\start_capture.jpg";
+        else imageSrc = webcamRef.current.getScreenshot();
 
         var a = document.createElement("a");
         a.style = "display: none";
@@ -50,26 +64,7 @@ function StartCheck2() {
           // 다운로드가 안되는 경우 방지
           document.body.removeChild(a);
         }, 100);
-      }, 10);
 
-      console.log(poseArr);
-
-      if (poseArr[0] == "1" || poseArr[0] == "-1") setPosture(-1);
-
-      if (poseArr[0] == "1" && count) {
-        window.localStorage.setItem("start", poseArr[0]);
-        window.localStorage.setItem("p1", poseArr[1]);
-        window.localStorage.setItem("p2", poseArr[2]);
-        window.localStorage.setItem("p3", poseArr[3]);
-        window.localStorage.setItem("p4", poseArr[4]);
-        window.localStorage.setItem("p5", poseArr[5]);
-
-        count = 0;
-      }
-    };
-
-    useEffect(() => {
-      setInterval(() => {
         capture();
       }, 5000);
     }, []);
@@ -127,10 +122,7 @@ function StartCheck2() {
           "양쪽 어깨와 눈이 보이도록 설정 후 10초 동안 바른 자세를 유지해주세요."}
 
         <div style={{ color: "red" }}>
-          {posture == -1 &&
-            window.localStorage.getItem("start") == "-1" &&
-            "바른 자세를 유지해주세요."}
-
+          {posture == -1 && "바른 자세를 유지해주세요."}
           {posture == 2 && "오른쪽 어깨가 확인되지 않았습니다."}
           {posture == 5 && "왼쪽 어깨가 확인되지 않았습니다."}
           {posture == 14 && "오른쪽 눈이 확인되지 않았습니다."}
@@ -153,6 +145,7 @@ function StartCheck2() {
             "오른쪽 어깨, 왼쪽 어깨, 오른쪽 눈, 왼쪽 눈이 확인되지 않았습니다."}
         </div>
       </div>
+      {/* 양쪽 눈과 어깨가 모두 파악된 바른 자세인 경우 자세 판별 페이지로 이동 */}
       {window.localStorage.getItem("start") == "1" && (
         <Redirect to={{ pathname: "/pose" }} />
       )}
